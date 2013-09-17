2790829008f0aa32959b89c37a42ef77cbd21703
confiz <confiz@confiz-1631.(none)>
2013-09-17 12:53:06 +0500

    minor changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require foundation
 //= require angular.min
+//= require angularjs/rails/resource
 //= require app
 //= require_tree ./angular
 //= require foundation-datepicker
@@ -21,7 +22,7 @@
 //= require_tree .
 
 $(function() {
-  $(document).foundation();
+	$(document).foundation();
 });
 
 
